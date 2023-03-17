6c145bcecaafb2a30f6e076e2425730eab4cb55b
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-03-17 14:50:05 +0530

    Create 01. Basic Intro Code.sql

---
+++ Lecture Code/01. Basic Intro Code.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE temp;
+
+USE temp;
+
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+INSERT INTO student VALUES(1, 'Ankit');
+
+SELECT * FROM student;
