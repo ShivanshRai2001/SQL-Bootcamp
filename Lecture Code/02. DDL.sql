b0f1c2d799ada1e67362a033871ef8d4e5c6545f
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-03-17 18:26:06 +0530

    Create 02. DDL.sql

---
+++ Lecture Code/02. DDL.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE temp;
+CREATE DATABASE IF NOT EXISTS temp;
+
+USE temp; -- need to execute to choose on which DB CREATE TABLE etc commands will be executed.
+-- make switching between DBs possible
+
+CREATE TABLE student ( -- puting content inside the database
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+INSERT INTO student VALUES(1, 'Ankit');
+
+SELECT * FROM student;
+
+SHOW DATABASE; -- list all the DBs in the server
+
+SHOW TABLE; -- list tables in the selected DB
+
+DROP DATABASE IF EXISTS temp; -- droping database
